f4d2a4c957d7305e18b7a1c5545eaae5360b08c4
dimitris <dimitrios.chatzopoulos@netcompany-intrasoft.com>
2023-05-28 00:36:13 +0300

    v

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -2,3 +2,16 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+$(document).ready(function() {
+  $('#dataTableForDessert').DataTable();
+});
+$(document).ready(function() {
+  $('#dataTableForLunch').DataTable();
+});
+$(document).ready(function() {
+  $('#dataTableForSnack').DataTable();
+});
+$(document).ready(function() {
+  $('#dataTableForDinner').DataTable();
+});
+
